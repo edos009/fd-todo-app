33394b100fa495b5fba535a0904d2a5b7d86ff5e
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-08-23 11:09:08 +0300

    Edit condition for reg

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -2,6 +2,6 @@ import * as Yup from "yup";
 
 export const schema_task = Yup.object({
   body: Yup.string("Input value must be string")
-  .matches(/^[A-Za-z0-9 ,.?!]{4,40}$/, "Invalid display name")
+  .matches(/^\w[A-Za-z0-9 ,.?!]{4,40}$/, "Invalid display name")
   .required("This field is required")
 })
